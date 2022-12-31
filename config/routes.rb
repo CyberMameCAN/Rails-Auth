c026c4d539156e8c4dae2d2459a2a2b6794f7aa7
Toshiya Takeshita <toshi@atbat.jp>
2022-12-31 16:21:28 +0900

    deviceの基礎まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'static_pages#home'
+  resources :tasks
+  # root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
